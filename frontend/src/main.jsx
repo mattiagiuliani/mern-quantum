a097b703722c94b9638f7c6f0d4c06f429cf3a70
mattiagiuliani <mattiaggt02@gmail.com>
2026-05-01 14:00:46 +0200

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/globals.css'
 import App from './App.jsx'
 import { initFrontendSentry } from './config/sentry'
-import { startPerfMonitoring } from './config/perf'
 
 initFrontendSentry()
-startPerfMonitoring()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
